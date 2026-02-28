91983f50abafbec67a38ff6d8302787532390751
Brwolf <brwolffan@gmail.com>
2026-02-28 01:53:00 -0300

    Refactor print statements and implement lexer
    Refactor print statements and add lex function for tokenization.

--- system.py
+++ system.py
@@ -1,5 +1,25 @@
 from math import sqrt
 print(sqrt(16))
 
-#ANOS
-print("AN", "OS")
+#AN OS
+print("AN", " OS")
+
+class Token():
+  def __init__(self, contents:str, type):
+    self.val = contents
+    self.type = type
+  
+
+def lex(string:str):
+  i = 0
+  while i < len(string):
+    if character.isspace():
+      i += 1
+      continue
+    elif character.isalpha()
+      I = 0
+      interestIndex = i
+      while character.isalnum() or character == "_" or character == "-":
+        i += 1
+        continue
+      yield Token(string[interestIndex:i], "identifier)
